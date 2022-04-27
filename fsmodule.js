5dcb1198aef25bf3c61a759b6e28e4e09a9fee57
ranashivam207 <ranashivam207@gmail.com>
2022-04-27 08:59:37 +0530

    readFileSync method

--- fsmodule.js
+++ fsmodule.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
-fs.readFile('file.txt', 'utf8',(err, data)=>{
-    console.log(err, data)
-})
+// fs.readFile is used to read data from the file
+//fs.readFile('file.txt', 'utf8',(err, data)=>{
+  //  console.log(err, data)
+//})
+
+const a = fs.readFileSync('file.txt')
+console.log(a.toString());
+
